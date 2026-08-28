e6527144ddfec3f40319eb648b160daca7d02971
bryanprimus <bryantobing0@gmail.com>
2026-08-28 17:33:40 +0800

    feat: add shadcn ui integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,19 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import umami from '@yeskunall/astro-umami';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  integrations: [
-    umami({
-      id: '011ffcdf-f9e9-49ad-8bf0-587e41429ccb'
-    })
-  ]
+
+  integrations: [umami({
+    id: '011ffcdf-f9e9-49ad-8bf0-587e41429ccb'
+  }), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
