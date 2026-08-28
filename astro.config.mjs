e503cf799c70f392697ad7f3d94a07f726de257a
bryanprimus <bryantobing0@gmail.com>
2026-08-28 17:01:32 +0800

    feat: add Umami analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import umami from '@yeskunall/astro-umami';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [
+    umami({
+      id: '011ffcdf-f9e9-49ad-8bf0-587e41429ccb'
+    })
+  ]
 });
